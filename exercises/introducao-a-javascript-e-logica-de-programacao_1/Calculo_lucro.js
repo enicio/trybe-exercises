1d07067c9aa48feb7f98c1a8ef1a3ef9591daaf2
enicio <jessandro42@gmail.com>
2020-10-24 19:29:45 -0300

    Alterado a lógica do arquivo Nota_em_conceito

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Calculo_lucro.js
@@ -0,0 +1,21 @@
+/*
+Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda.
+A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil 
+desses produtos.
+Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% 
+também faz parte do valor de custo.
+
+valorCustoTotal = valorCusto + impostoSobreOCusto
+
+lucro = valorVenda - valorCustoTotal (lucro de um produto)
+*/
+
+let CustoProd = 0;
+let ValorVenda = 0;
+let lucro = 0;
+
+
--- exercises/introducao-a-javascript-e-logica-de-programacao_1/Notas_em_Conceitos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Notas_em_Conceitos.js
@@ -9,21 +9,31 @@ Porcentagem < 50 -> F
 O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 */
 
-let nota = 92;
+ let nota = 42;
 
-if (nota > 100 || nota < 0 ) {
-    console.log("Nota invalida");
-} else {
-if(nota >= 90) {
-    console.log("A")
-} else if ( nota >= 80) {
-    console.log("B")
-} else if ( nota >= 70) {
-    console.log("C")
-} else if ( nota >= 60) {
-    console.log("D")
-} else if ( nota >= 50) {
-    console.log("E")
-} else { console.log("F") }
+ if (nota > 100 || nota < 0 ) {
+     console.log("Nota invalida");
+ } else {
+
+
+// if(nota >= 90) {
+//     console.log("A")
+// } else if ( nota >= 80) {
+//     console.log("B")
+// } else if ( nota >= 70) {
+//     console.log("C")
+// } else if ( nota >= 60) {
+//     console.log("D")
+// } else if ( nota >= 50) {
+//     console.log("E")
+// } else { console.log("F") }
+
+if(nota >= 90) return console.log("A")
+if(nota >= 80) return console.log("B")
+if(nota >= 70) return console.log("C")
+if(nota >= 60) return console.log("D")
+if(nota >= 50) return console.log("E")
+if(nota < 50) return console.log("F")
 
 }
+

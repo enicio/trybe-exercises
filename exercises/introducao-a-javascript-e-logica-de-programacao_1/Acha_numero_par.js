7bfa7c45164333800e9a0950ce2bcc381dde9894
enicio <jessandro42@gmail.com>
2020-10-24 01:01:40 -0300

    Acha se tem numero pat

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Acha_numero_par.js
@@ -0,0 +1,13 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos
+// uma das três for par. Caso contrário, ele retorna false.
+
+
+let a = 1;
+let b = 5;
+let c = 6;
+
+if ( a%2==0 || b%2==0 || c%2==0) {
+    console.log("True");
+} else {
+    console.log("False");
+}

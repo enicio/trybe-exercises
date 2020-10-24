7bc8c37dc423333edc644d3c0ea01a990d1151a9
enicio <jessandro42@gmail.com>
2020-10-24 01:06:43 -0300

    Acha se tem numero impar

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Acha_numero_impar.js
@@ -0,0 +1,13 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo 
+// menos uma das três for ímpar. Caso contrário, ele retorna false.
+
+
+let a = 3;
+let b = 8;
+let c = 6;
+
+if ( a%2!=0 || b%2!=0 || c%2!=0) {
+    console.log("True");
+} else {
+    console.log("False");
+}

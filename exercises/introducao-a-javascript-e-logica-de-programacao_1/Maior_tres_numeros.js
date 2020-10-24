a1451bd14ba5cc6c37d05c54c71c7302785bf4cf
enicio <jessandro42@gmail.com>
2020-10-23 23:20:23 -0300

    Acha positivo e negativo

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/Maior_tres_numeros.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Maior_tres_numeros.js
@@ -1,9 +1,9 @@
 // Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os
 // valores que serão comparados.
 
-let a = 7;
+let a = 755;
 let b = 9;
-let c = 8;
+let c = 81;
 let aux;
 
 if (a > b) {
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Positivo_Negativo.js
@@ -0,0 +1,10 @@
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" 
+// se for negativo e "zero" caso contrário.
+
+let a = 0;
+
+if ( a > 0 ) {
+    console.log("Positivo");
+} else if ( a < 0) {
+    console.log("Negativo");
+}else ( console.log("Zero"))

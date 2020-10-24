0512e503dd12923047aff699321f7bfb1c78fd60
enicio <jessandro42@gmail.com>
2020-10-23 22:49:50 -0300

    4 operações basicas

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/adicao.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b =  7;
+
+adi = a + b;
+sub = a - b;
+mult = a * b;
+div = a / b;
+mod = a % b;
+
+console.log("A soma", adi);
+console.log("A subtração",sub);
+console.log("O produto",mult);
+console.log("O quociente",div);
+console.log("O módulo", mod);

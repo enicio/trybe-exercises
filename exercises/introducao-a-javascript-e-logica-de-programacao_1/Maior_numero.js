b3dfe5306deb9f5974995b20b0fea8c135debdd1
enicio <jessandro42@gmail.com>
2020-10-23 23:03:13 -0300

    Acha o maior entre dois numeros

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Maior_numero.js
@@ -0,0 +1,12 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os 
+// valores que serão comparados.
+
+
+let a = 12;
+let b = 15;
+
+if ( (a - b) > 0 ) {
+    console.log(a);
+} else {
+    console.log(b);
+}
--- exercises/introducao-a-javascript-e-logica-de-programacao_1/adicao.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/adicao.js
@@ -1,3 +1,6 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, 
+// definidas no começo com os valores que serão operados. Faça programas para: Adição, Subtração, Multiplicação, Divisão,Módulo
+
 let a = 5;
 let b =  7;
 

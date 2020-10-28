88548b7a70dce91e25f0f222d6e97519e960cf11
enicio <jessandro42@gmail.com>
2020-10-27 22:34:56 -0300

    Os tres primeiros

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/for_in_mostra_valores.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "sim"
+  };
+
+  for (dados in info) {
+        console.log(dados)
+
+  }
